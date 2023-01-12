b24b743b75149dd92e6840d015a3806bec9310b8
SniperBones <morenohurtadoalejandro@gmail.com>
2023-01-12 02:37:03 -0600

    Azure Functions Locales

---
+++ Cliente-Servidor/Af/Tienda/Cargar.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Tienda
+{
+    public static class Cargar
+    {
+        [FunctionName("Cargar")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+                // la variable de entorno HOME está predefinida en el servidor (C:\home o D:\home)
+                // Los archivos cargados se pueden ver en el portal, seleccionando la aplicación de funciones y la opción "Consola" en la sección "Herramientas de desarrollo"
+                // En el servidor no se puede escribir en el directorio c:\home\site\wwwroot
+                // Para que la aplicación "carga.html" pueda acceder a la función "Cargar" es necesario habilitar CORS en el portal de Azure.
+
+                string home = Environment.GetEnvironmentVariable("HOME");
+
+                string ruta = data.ruta;
+                string nombre = data.nombre;
+                byte[] contenido = Convert.FromBase64String((string)data.contenido);
+
+                // si no existe el directorio correspondiente a la ruta, lo crea
+                if (!Directory.Exists(home + "/data" + ruta))
+                    Directory.CreateDirectory(home + "/data" + ruta);
+
+                File.WriteAllBytes(home + "/data" + ruta + "/" + nombre, contenido);
+                return new OkObjectResult("OK");
+            }
+            catch (Exception e)
+            {
+                return new BadRequestObjectResult(e.Message);
+            }
+        }
+    }
+}
---
+++ Cliente-Servidor/Af/Tienda/Class.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+using System.IO;
+using Newtonsoft.Json;
+
+namespace FunctionApp1
+{
+    public static class Get
+    {
+        [FunctionName("Get")]
+        [ResponseCache(Duration = 86400, NoStore = false)]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                string path = (string)req.Query["nombre"];
+
+                // el parámetro "descargar" es opcional, por default es "NO"
+                bool descargar = ((string)req.Query["descargar"] ?? "NO").ToUpper() == "SI";
+
+                // la variable de entorno HOME está predefinida en el servidor (C:\home o D:\home)
+                // Los archivos cargados se pueden ver en el portal, seleccionando la aplicación de funciones y 
+                // la opción "Consola" en la sección "Herramientas de desarrollo"
+
+                string home = Environment.GetEnvironmentVariable("HOME");
+
+                try
+                {
+                    byte[] contenido = File.ReadAllBytes(home + "/data" + path);
+                    string nombre = Path.GetFileName(path);
+                    string tipo_mime = MimeMapping.GetMimeMapping(path);
+                    DateTime fecha_modificacion = File.GetLastWriteTime(home + "/data" + path);
+
+                    if (descargar)
+                        // el navegador descargará el contenido como archivo
+                        return new FileContentResult(contenido, tipo_mime) { FileDownloadName = nombre };
+                    else
+                        // el navegador mostrará el contenido
+                        // se envia al navegador el tipo mime para que lo procese adecuadamente
+                        // se envia al navegador la fecha de modificación del archivo para usar HTTP caching
+                        return new FileContentResult(contenido, tipo_mime) { LastModified = fecha_modificacion };
+                }
+                catch (FileNotFoundException)
+                {
+                    return new NotFoundResult();
+                }
+            }
+            catch (Exception e)
+            {
+                return new BadRequestObjectResult(e.Message);
+            }
+        }
+    }
+}
